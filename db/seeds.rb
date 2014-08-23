ff6eaf87bc4e8a9ddc939d52322ffff0d7ec5876
Mikael Svensson <github@mikaelsvensson.info>
2014-08-23 12:08:48 +0200

    Seed data to populate database with default categories #4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create([
+                    {group: "scout-concept", name: "Andakt, stilla stund"},
+                    {group: "scout-concept", name: "Ceremonier"},
+                    {group: "scout-concept", name: "Diskussion"},
+                    {group: "scout-concept", name: "Friluftsliv land"},
+                    {group: "scout-concept", name: "Friluftsliv sjö"},
+                    {group: "scout-concept", name: "Hajk"},
+                    {group: "scout-concept", name: "Hantverk, pyssel"},
+                    {group: "scout-concept", name: "Lekar"},
+                    {group: "scout-concept", name: "Lägerbål"},
+                    {group: "scout-concept", name: "Matlagning"},
+                    {group: "scout-concept", name: "Samhällsengagemang"},
+                    {group: "scout-concept", name: "Teater, musik, sång"},
+                    {group: "scout-concept", name: "Utomhus"},
+
+                    {group: "scout-track", name: "Aktiv i gruppen"},
+                    {group: "scout-track", name: "Aktiv i samhället"},
+                    {group: "scout-track", name: "Egna värderingar"},
+                    {group: "scout-track", name: "Existens"},
+                    {group: "scout-track", name: "Fantasi och kreativt uttryck"},
+                    {group: "scout-track", name: "Fysiska utmaningar"},
+                    {group: "scout-track", name: "Förståelse för omvärlden"},
+                    {group: "scout-track", name: "Kritiskt tänkande"},
+                    {group: "scout-track", name: "Känsla för naturen"},
+                    {group: "scout-track", name: "Ledarskap"},
+                    {group: "scout-track", name: "Problemlösning"},
+                    {group: "scout-track", name: "Relationer"},
+                    {group: "scout-track", name: "Självinsikt och självkänsla"},
+                    {group: "scout-track", name: "Ta hand om sin kropp"}
+                ])
