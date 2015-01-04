833cff6ed002f313b9279c70444653de7fcd99cc
Mikael Svensson <github@mikaelsvensson.info>
2015-01-04 15:43:31 +0100

    Category images #31

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,12 +1,23 @@
+require 'db_constants'
 module Api
   module V1
+    include Db
     class CategoriesController < ApplicationController
       before_filter :restrict_access_to_api_users, except: [:index, :show]
       before_action :set_category, only: [:show, :update, :destroy]
+      before_action :load_usage_count, only: [:index, :show]
 
       def index
-        @categories = Category.all.order(:group => :asc, :name => :asc)
+        @categories = Category.all.includes(:media_file).order(:group => :asc, :name => :asc)
         respond_with @categories
+      end
+
+      def load_usage_count
+        @usageCount = Category.
+            joins(:activity_versions).
+            where("activity_versions.status = ?", Db::ActivityVersionStatus::PUBLISHED).
+            group(:category_id).
+            count(:activity_versions)
       end
 
       def create
@@ -39,7 +50,7 @@ module Api
         elsif !params[:media_file_uri].nil?
           file = MediaFile.find_by_uri(params[:media_file_uri])
           if file.nil?
-            file = MediaFile.new({ :uri => params[:media_file_uri] })
+            file = MediaFile.new({:uri => params[:media_file_uri]})
           end
           file
         else
--- app/views/api/v1/categories/_category.json.jbuilder
+++ app/views/api/v1/categories/_category.json.jbuilder
@@ -7,6 +7,10 @@ json.extract! category,
               :updated_at
 #json.group category.group.force_encoding("ISO-8859-1")
 #json.name category.name.force_encoding("ISO-8859-1")
+
+# Number of users who have marked the activity as a favourite
+json.activities_count @usageCount[category.id].nil? ? 0 : @usageCount[category.id]
+
 json.media_file do
   json.partial! category.media_file
 end unless category.media_file.nil?
